d1c46d5363b21169e69755ab6ab624df0418bb84
Nathanael Swander <swander926@yahoo.com>
2019-11-26 17:38:23 -0500

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import ShowPage from './pages/ShowPage'
-import NotFound from './pages/NotFound'
 
 const App = () => {
   return <HomePage />
