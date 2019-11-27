3bf021688cd001d1481c904059867bc956c0c63b
Nathanael Swander <swander926@yahoo.com>
2019-11-26 19:03:00 -0500

    data displayed on page

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
-const Shows = () => {
-  return <div>Hello, World!</div>
+const Shows = props => {
+  // const filePath = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'
+
+  return (
+    <>
+      <div>
+        <section>
+          <li>{props.key}</li>
+          <li>{props.name}</li>
+          <li>
+            <img
+              src={
+                'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + props.img
+              }
+            />
+          </li>
+          <li>{props.overview}</li>
+        </section>
+      </div>
+    </>
+  )
 }
 
 export default Shows
