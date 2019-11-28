0383f592f131b5adc4bafea6cf7f592bf2eaa064
amgad behman <amgadbehman12345@gmail.com>
2019-11-27 16:10:45 -0800

    linkworking

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Shows = props => {
   // const filePath = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'
-
   return (
     <>
       <div className="tvContainer">
         <section className="tvSection">
-          <h2>{props.name}</h2>
+          <Link to="/:id">
+            <h2>{props.name}</h2>
+          </Link>
           <ul className="tvList">
             <li>{props.key}</li>
             <li>
-              <img
-                src={
-                  'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + props.img
-                }
-                height="400px"
-                width="300px"
-                alt="TV Poster Image"
-              />
+              <Link to="/:id">
+                <img
+                  src={
+                    'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' +
+                    props.img
+                  }
+                  height="400px"
+                  width="300px"
+                  alt="TV Poster Image"
+                />
+              </Link>
             </li>
             <li>{props.overview}</li>
           </ul>
@@ -27,5 +31,4 @@ const Shows = props => {
     </>
   )
 }
-
 export default Shows
