528f457106a505b06e58b93dc1c2529fa1688544
amgad behman <amgadbehman12345@gmail.com>
2019-11-29 14:39:18 -0800

    highlight aND LINK

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Shows = props => {
   // const filePath = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'
-  return (
-    <>
-      <div className="tvContainer">
+  if (props.random == props.id) {
+    return (
+      <div className="randomTvContainer">
         <div className="tvBackground">
           <section className="tvSection">
             <ul className="tvList">
@@ -40,14 +40,12 @@ const Shows = props => {
     )
   } else {
     return (
-      <>
-        <div className="tvContainer">
+      <div className="tvContainer">
+        <div className="tvBackground">
           <section className="tvSection">
-            <Link to={`/${props.id}`}>
-              <h2>{props.name}</h2>
-            </Link>
             <ul className="tvList">
               <li>{props.key}</li>
+              {/* what is props.key */}
               <li>
                 <Link to={`/${props.id}`}>
                   <img
@@ -61,11 +59,19 @@ const Shows = props => {
                   />
                 </Link>
               </li>
-              <li>{props.overview}</li>
             </ul>
+            <Link to={`/${props.id}`}>
+              <h2>{props.name}</h2>
+            </Link>
+            <p className="paraGraph">{props.overview}</p>
+            <br></br>
+            <br></br>
           </section>
+          <hr className="breakline" width="700px"></hr>
+          <br></br>
+          <br></br>
         </div>
-      </>
+      </div>
     )
   }
 }
